54703094284be3741eda72532e5ea25b00e3beb2
avangersKinkyClient <63201809+avangersKinkyClient@users.noreply.github.com>
2020-08-30 23:03:34 +0200

    Update GlobalKeyBoardHook.cs

--- OmegaProject/OmegaProject/GlobalKeyBoardHook.cs
+++ OmegaProject/OmegaProject/GlobalKeyBoardHook.cs
@@ -11,6 +11,7 @@ namespace OmegaProject
 {
     public class GlobalKeyBoardHook
     {
+        // multiple dll imports for the auto clicker and other perposes
         [DllImport("user32.dll")]
         static extern int CallNextHookEx(IntPtr hhk, int code, int wParam, ref keyBoardHookStruct lParam);
         [DllImport("user32.dll")]
@@ -19,20 +20,21 @@ namespace OmegaProject
         static extern bool UnhookWindowsHookEx(IntPtr hInstance);
         [DllImport("kernel32.dll")]
         static extern IntPtr LoadLibrary(string lpFileName);
-
+        // delegate int for impelmenting the load library key board hooking
         public delegate int LLKeyboardHook(int Code, int wParam, ref keyBoardHookStruct lParam);
-
+        // int's for calc and call the presses clicks
         const int WH_KEYBOARD_LL = 13;
         const int WM_KEYDOWN = 0x0100;
         const int WM_KEYUP = 0x0101;
         const int WM_SYSKEYDOWN = 0x0104;
         const int WM_SYSKEYUP = 0x0105;
-
+        // a load library key board hooking call
         LLKeyboardHook llkh;
+        // generate the hooked key list 
         public List<Keys> HookedKeys = new List<Keys>();
 
         IntPtr Hook = IntPtr.Zero;
-
+        // public events for calc the clicks if get presses 
         public event KeyEventHandler KeyDown;
         public event KeyEventHandler KeyUp;
 
@@ -44,17 +46,20 @@ namespace OmegaProject
         ~GlobalKeyBoardHook()
         { unhook(); }
 
+        // hook the autoclicker
         public void hook()
         {
             IntPtr hInstance = LoadLibrary("User32");
             Hook = SetWindowsHookEx(WH_KEYBOARD_LL, llkh, hInstance, 0);
         }
 
+        // unhook the autoclicker
         public void unhook()
         {
             UnhookWindowsHookEx(Hook);
         }
 
+        // function for hooking the keybind processes
         public int HookProc(int Code, int wParam, ref keyBoardHookStruct lParam)
         {
             if (Code >= 0)
@@ -75,6 +80,7 @@ namespace OmegaProject
         }
     }
 
+    // structure for hooking the keyboard shortcuts
     public struct keyBoardHookStruct
     {
         public int vkCode;
